ab50a168d772e856ee6bcde8cdf23ffcf876ad57
gpablo6 <gpablo6@outlook.com>
2022-10-11 12:23:14 -0500

    Latest revision for script

--- src/main.py
+++ src/main.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
                     'value': f'#{i}'})
 
         print(f'Writting file #{i}\n')
-        with open(f'{base_route}/metadata/{i}.json', 'w+') as w:
+        with open(f'{base_route}/metadata/{i}', 'w+') as w:
             json.dump(temp, w)
     
     print("Done writting files!\n")
